66cf7108e61a65a47b5c62fd66e7d16a20844583
Anton Vtorov <anton.vtorov90@gmail.com>
2023-05-25 18:06:48 +0300

    reb

---
+++ 6.5.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+time_t time(time_t* timer);
+
+int main()
+	{
+	 
+	///
+	int total, h, m, s;
+	return 0;
+	}
+//1684940766
