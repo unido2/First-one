c69b0922f58f5a94194ac52d4c132aa0e59b9181
Anton Vtorov <anton.vtorov90@gmail.com>
2023-07-06 12:25:21 +0300

    Complex

--- 6.6.cpp
+++ 6.6.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Complex.h"
-#include <math.h>
 using namespace std;
 
 int main() {
--- Complex.cpp
+++ Complex.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "Complex.h"
 #include <math.h>
-using namespace std;
+
 
 Complex::Complex(double a1,double b1, double a2, double b2) {
 	MemNUm[0][0] = a1;
@@ -17,18 +17,18 @@ void Complex::setComplex() {
 		count++;
 
 		if (count == 1) {
-			cout << "vvedite z1=a1+b1i: " << endl << "a1= ";
-			cin >> MemNUm[0][0];
-			cout << "b1= ";
-			cin >> MemNUm[0][1];
-			cout << endl;
+			std::cout << "vvedite z1=a1+b1i: " << std::endl << "a1= ";
+			std::cin >> MemNUm[0][0];
+			std::cout << "b1= ";
+			std::cin >> MemNUm[0][1];
+			std::cout << std::endl;
 		}
 		else {
-			cout << "vvedite z2=a2+b2i: " << endl << "a2= ";
-			cin >> MemNUm[1][0];
-			cout << "b2= ";
-			cin >> MemNUm[1][1];
-			cout << endl;
+			std::cout << "vvedite z2=a2+b2i: " << std::endl << "a2= ";
+			std::cin >> MemNUm[1][0];
+			std::cout << "b2= ";
+			std::cin >> MemNUm[1][1];
+			std::cout << std::endl;
 		}
 	} while (count != 2);
 }
@@ -44,8 +44,8 @@ void Complex::subsComolex() {
 }
 
 void Complex::print() {
-	cout << "Summa compleksnix chisel z = " << aSum <<
-		(bSum<0?" - ":" + ") << abs(bSum) << "i" << endl;
-	cout << "Raznica compleksnix chisel z = " << aSub <<
-		(bSub < 0 ? " + " : " - ") << abs(bSub) << "i" << endl;
+	std::cout << "Summa compleksnix chisel z = " << aSum <<
+		(bSum<0?" - ":" + ") << abs(bSum) << "i" << std::endl;
+	std::cout << "Raznica compleksnix chisel z = " << aSub <<
+		(bSub < 0 ? " + " : " - ") << abs(bSub) << "i" << std::endl;
 }
